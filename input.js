b5ffd0a1e078f5b5605515699488cfe3b0f3d0d8
Julius-717 <waynejulius10@gmail.com>
2019-12-18 23:35:10 +0300

    commit

---
+++ input.js
@@ -0,0 +1,29 @@
+(function() {
+    var pressedKeys = {};
+
+    function setKey(event, status) {
+        var code = event.keycode;
+        var key;
+
+        switch (code) {
+            case 32:
+                key = "SPACE";
+                break;
+            case 37:
+                key = "LEFT";
+                break;
+            case 38:
+                key = "UP";
+                break;
+            case 39:
+                key = "RIGHT";
+                break;
+            case 40:
+                key = "DOWN";
+                break;
+            default:
+                // Convert ASCII codes to letters
+                key = String.fromCharCode(code);
+        }
+    }
+})
