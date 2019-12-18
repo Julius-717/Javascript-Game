7f1522fb8aa55737b4a07b8f2c6ce5085a7c52d3
Julius-717 <waynejulius10@gmail.com>
2019-12-18 20:02:09 +0300

    add a callback function

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ var requestAnimFrame = (function(){
         window.mozRequestAnimationFrame    ||
         window.oRequestAnimationFrame      ||
         window.msRequestAnimationFrame     ||
-        
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        }
 })
