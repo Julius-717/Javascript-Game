7006537c6ea902b0001426c2c06f09e260250195
Julius-717 <waynejulius10@gmail.com>
2019-12-18 23:39:27 +0300

    commit

--- input.js
+++ input.js
@@ -25,5 +25,25 @@
                 // Convert ASCII codes to letters
                 key = String.fromCharCode(code);
         }
+
+        pressedKeys[key] = status;
     }
-})
+
+    document.addEventListener("keydown", function(e) {
+        setKey(e, true);
+    });
+
+    document.addEventListener("keyup", function(e) {
+        setKey(e, false);
+    });
+
+    window.addEventListener("blur", function() {
+        pressedKeys = {};
+    });
+
+    window.input = {
+        isDown: function(key) {
+            return pressedKeys[key.toUpperCase()];
+        }
+    };
+})();
