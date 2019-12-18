311d1efda1726a7a8fe67b98bc72eba54026a2dd
Julius-717 <waynejulius10@gmail.com>
2019-12-18 22:52:32 +0300

    add explosion

--- main.js
+++ main.js
@@ -207,6 +207,19 @@ function checkCollisions() {
                 i--;
 
                 score += 100;
+
+                explosions.push({
+                    pos: pos,
+                    sprite: new Sprite(
+                        "img/sprites.png",
+                        [0, 117],
+                        [39, 39],
+                        16,
+                        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                        null,
+                        true
+                    )
+                })
             }
         }
     }
