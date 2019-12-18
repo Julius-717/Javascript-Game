1a9d6ee655d8887ae69595377c20b127075a4dde
Julius-717 <waynejulius10@gmail.com>
2019-12-18 20:00:40 +0300

    add variable function whicn requests a cross-browser animationframe

---
+++ main.js
@@ -0,0 +1,8 @@
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        
+})
