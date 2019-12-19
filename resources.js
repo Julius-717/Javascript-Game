da5f2e65b52465666b903cd2aa26c4cbeb953e8f
Julius-717 <waynejulius10@gmail.com>
2019-12-19 15:32:50 +0300

    add function to load an image url or an array of image urls

--- resources.js
+++ resources.js
@@ -2,4 +2,16 @@
     var resourceCache = {};
     var loading = [];
     var readyCallbacks = [];
+
+
+    function load(urlOrArr) {
+        if (urlOrArr instanceof Array) {
+            urlOrArr.forEach(function(url) {
+                _load(url);
+            });
+        } else {
+            _load(urlOrArr);
+        }
+    }
+    
 })
